1f2690b756557114b80d04a8dd683a55ad0a6f08
Uygar Eren <komifb90@hotmail.com>
2023-04-24 02:01:55 +0300

    done

---
+++ app.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text-typing",{
+    strings : ["Student",
+            "Teacher",
+            "Developer",
+            "Doctor"],
+    typeSpeed : 90,
+    backSpeed: 100,
+    loop: true        
+                        
+})
